6b5310a90233ee2b9ee1aeed312936a7511e65d2
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2019-10-24 04:31:44 -0400

    fibonacci

---
+++ Fibonacci/Fibonacci.hs
@@ -0,0 +1,27 @@
+module Fibonacci
+       ( fibonacci
+       )
+where
+
+import Text.Format
+
+fibonacci :: Int -> Int
+fibonacci n =
+    if n < 0
+    then error (format "{0} is less than 0" [show n])
+    else
+        if n <= 1
+        then n
+        else (fibonacci (n - 1)) + (fibonacci (n - 2))
+
+printTimes :: Int -> IO()
+printTimes 0 = do putStrLn (format "Fib of {0}: {1}" [show 0, show (fibonacci 0)])
+printTimes n = do 
+    printTimes (n - 1)
+    putStrLn (format "Fib of {0}: {1}" [show n, show (fibonacci n)])
+
+main :: IO()
+main = do
+    printTimes 9
+
+    
